d054451fac8ff70782a5fb4fc14892947fbacf1d
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-09 23:59:17 -0500

    implement initial banner links

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,11 +8,6 @@ import Banner from '../Banner/Banner';
 import AboutBannerText from './AboutBannerText';
 import hill from '../../assets/img/hills-1.jpg';
 
-const bannerText = {
-  h1: "About Me",
-  p: "Find about a bit about this guy over here!"
-};
-
 const About = () =>
   <div id="about-page">
     <Header/>
---
+++ src/components/Banner/BannerLinks.jsx
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+const BannerLinks = () =>
+  <div id="banner-links-container">
+    <div id="banner-links">
+      <div className="banner-link">
+        <span><a href="https://www.github.com/ooronning"><div className="banner-link-div">Follow me on Github</div></a></span>
+      </div>
+      <div className="banner-link">
+        <span><a href="https://www.linkedin.com/in/christopherronning"><div className="banner-link-div">Add me on LinkedIn</div></a></span>
+      </div>
+      <div className="banner-link">
+        <span><a href="http://stackoverflow.com/story/christopher-ronning"><div className="banner-link-div">See my Stack Overflow</div></a></span>
+      </div>
+    </div>
+  </div>
+
+export default BannerLinks;
