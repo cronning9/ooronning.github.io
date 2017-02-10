d1f68127cfc7691eb33f4f49913317778c3678df
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-10 14:43:18 -0500

    change link display on resize

--- src/components/Banner/BannerLinks.jsx
+++ src/components/Banner/BannerLinks.jsx
@@ -9,18 +9,30 @@ const BannerLinks = () =>
     <div id="banner-links">
       <div className="banner-link-row">
         <div className="banner-link">
-          <a href="https://www.github.com/ooronning"><span className="banner-link-span">Follow me on Github</span></a>
+          <a href="https://www.github.com/ooronning">
+            <span className="banner-link-default">Follow me on Github</span>
+            <span className="banner-link-small">Test</span>
+          </a>
         </div>
         <div className="banner-link">
-          <a href="https://www.linkedin.com/in/christopherronning"><span className="banner-link-span">Add me on LinkedIn</span></a>
+          <a href="https://www.linkedin.com/in/christopherronning">
+            <span className="banner-link-default">Add me on LinkedIn</span>
+            <span className="banner-link-small">Test</span>
+          </a>
         </div>
       </div>
       <div className="banner-link-row">
         <div className="banner-link">
-          <a href="http://stackoverflow.com/story/christopher-ronning"><span className="banner-link-span">See my Stack Overflow</span></a>
+          <a href="http://stackoverflow.com/story/christopher-ronning">
+            <span className="banner-link-default">See my Stack Overflow</span>
+            <span className="banner-link-small">Test</span>
+          </a>
         </div>
         <div className="banner-link">
-          <Link to="portfolio"><span className="banner-link-span">View My Portfolio</span></Link>
+          <Link to="portfolio">
+            <span className="banner-link-default">View My Portfolio</span>
+            <span className="banner-link-small">Test</span>
+          </Link>
         </div>
       </div>
     </div>
