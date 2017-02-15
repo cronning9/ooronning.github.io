60c9763728494ee663c9f77b20240fa82ecdb596
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-14 19:27:54 -0500

    server fixes

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const morgan = require('morgan');
 const path = require('path');
 
 app.use(morgan('dev'));
+app.use(express.static('./build'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(3000, () => {
   console.log(`now listening on ${listener.address().port}`)
 });
