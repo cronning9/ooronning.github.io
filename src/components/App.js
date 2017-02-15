43af4c3a4fe61050883bdbe6707521f0863d38d1
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-14 21:05:29 -0500

    add a footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@
 import React from 'react';
 
 import Header from './Header/Header';
+import BottomBar from './BottomBar';
 
 const App = ({children}) =>
   <div className="app">
     <Header />
     {children}
+  <BottomBar/>
   </div>
 
 export default App;
