e4f67ff8c61e297891297fc75f7f4673dbeccaae
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-23 15:16:46 -0500

    initial set-up for state machine

--- source/src/components/Portfolio/ContentShowcaseSlider.jsx
+++ source/src/components/Portfolio/ContentShowcaseSlider.jsx
@@ -4,9 +4,15 @@ import React from 'react';
 
 import Slide from './Slide';
 
+import projects from './projects';
+
 export default class ContentShowcaseSlider extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      slideIndex: 0
+    }
   }
 
   render() {
@@ -15,6 +21,8 @@ export default class ContentShowcaseSlider extends React.Component {
         <Slide title="Example"
                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
                link="https://www.example.com"/>
+        <i className="fa fa-chevron-left fa-1" aria-hidden="true"></i>
+        <i className="fa fa-chevron-right fa-1" aria-hidden="true"></i>
       </div>
     )
   }
