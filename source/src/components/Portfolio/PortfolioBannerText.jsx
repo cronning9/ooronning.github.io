ba8b72e97835a7d5f2dec7335c6bd308b8a84003
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-23 12:57:49 -0500

    Create Portfolio component and mount it to Router

---
+++ source/src/components/Portfolio/PortfolioBannerText.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+
+import BannerText from '../Banner/BannerText';
+
+const PortfolioBannerText = () => <BannerText title="Portfolio"
+                                              subtitle="Highlights of Projects I've Worked On"/>
+
+export default PortfolioBannerText;
