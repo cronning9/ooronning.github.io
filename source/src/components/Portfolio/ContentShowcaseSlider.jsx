19f210a3c3be3c1e56862a7fd987c69abfdd6784
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-23 15:42:01 -0500

    position arrows correctly

--- source/src/components/Portfolio/ContentShowcaseSlider.jsx
+++ source/src/components/Portfolio/ContentShowcaseSlider.jsx
@@ -18,11 +18,15 @@ export default class ContentShowcaseSlider extends React.Component {
   render() {
     return (
       <div id="showcase-slider">
+        <div className="slider-arrow" id="left-arrow">
+          <i className="fa fa-chevron-left fa-1" aria-hidden="true"></i>
+        </div>
         <Slide title="Example"
-               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+               description="Lorem ipsum dolor sit amet, cons  ectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
                link="https://www.example.com"/>
-        <i className="fa fa-chevron-left fa-1" aria-hidden="true"></i>
-        <i className="fa fa-chevron-right fa-1" aria-hidden="true"></i>
+        <div className="slider-arrow" id="right-arrow">
+          <i className="fa fa-chevron-right fa-1" aria-hidden="true"></i>
+        </div>
       </div>
     )
   }
