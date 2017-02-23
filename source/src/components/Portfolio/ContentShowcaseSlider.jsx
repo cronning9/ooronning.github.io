4833d8639e23453214db036f2b10b9cbead9c4c1
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-02-23 16:10:00 -0500

    slide shift core functionality works

--- source/src/components/Portfolio/ContentShowcaseSlider.jsx
+++ source/src/components/Portfolio/ContentShowcaseSlider.jsx
@@ -3,28 +3,43 @@
 import React from 'react';
 
 import Slide from './Slide';
-
 import projects from './projects';
 
 export default class ContentShowcaseSlider extends React.Component {
   constructor() {
     super();
-
     this.state = {
       slideIndex: 0
-    }
+    };
+
+    this.nextSlide = this.nextSlide.bind(this);
+    this.prevSlide = this.prevSlide.bind(this);
+  }
+
+  prevSlide() {
+    const currentSlide = this.state.slideIndex;
+    const nextSlide = currentSlide === 0 ? projects.length - 1 : currentSlide - 1;
+    this.setState({slideIndex: nextSlide});
+  }
+
+  nextSlide() {
+    const currentSlide = this.state.slideIndex;
+    const nextSlide = currentSlide === projects.length - 1 ? 0 : currentSlide + 1;
+    this.setState({slideIndex: nextSlide});
   }
 
   render() {
+    const slide = projects[this.state.slideIndex];
+
     return (
       <div id="showcase-slider">
-        <div className="slider-arrow" id="left-arrow">
+        <div className="slider-arrow" id="left-arrow" onClick={this.prevSlide}>
           <i className="fa fa-chevron-left fa-1" aria-hidden="true"></i>
         </div>
-        <Slide title="Example"
-               description="Lorem ipsum dolor sit amet, cons  ectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-               link="https://www.example.com"/>
-        <div className="slider-arrow" id="right-arrow">
+        <Slide title={slide.title}
+               description={slide.description}
+               link={slide.link}/>
+        <div className="slider-arrow" id="right-arrow" onClick={this.nextSlide}>
           <i className="fa fa-chevron-right fa-1" aria-hidden="true"></i>
         </div>
       </div>
--- source/src/components/Portfolio/projects.js
+++ source/src/components/Portfolio/projects.js
@@ -10,6 +10,11 @@ const projects = [
     title: "Example 2",
     description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     link: "http://www.example.org"
+  },
+  {
+    title: "Example 3",
+    description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    link: "http://en.wikipedia.org"
   }
 ];
 
